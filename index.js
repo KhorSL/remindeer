a24699f46dd7790195954b8aea4eb959b363e918
Khor Shao Liang <khorshaoliang@Khors-Air.home>
2017-07-27 18:57:18 +0800

    changes to listening port and host

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 
 const Telegram = require('telegram-node-bot');
 const tg = new Telegram.Telegram('433728794:AAHYqC2qaD88yIW5eekKbMYBVfisdRK2Hmo', {
-	workers: 1 //default uses as many as the CPU calls
+	workers: 1 //default uses as many as the CPU calls,
+	webAdmin: {
+        port: process.env.PORT || 7777,
+        host: process.env.HOST || 'localhost'
+    }
 });
 
 const RemindController = require('./controllers/remind');
